75962c654fc7845717dc910a927d4fc9ebd5e394
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-04-15 19:45:46 +0200

    Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Profile } from "./Profile/user_information";
+import user from "./Profile/user.json";
+
+
+
 export const App = () => {
   return (
-    <div
+     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        padding: 20,
+        color: '#010101',
       }}
     >
-      React homework template
-    </div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+       </div>
   );
 };
