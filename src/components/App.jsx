472c8ae9f3a84d14dbed35a0fdc1d5c587b89d79
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-04-16 19:23:10 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,9 @@ import { Statistics } from './Statistics/Statistics';
 import data from './Statistics/data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
- 
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json'
+
 export const App = () => {
   return (
     <div
@@ -28,6 +30,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
-  );
-};
+  )
+}
